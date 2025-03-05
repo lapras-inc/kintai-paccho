2aa0fb17f7b5a4d4dbe6f6b24fc2606d9233ee96
Shuhei Yamashita <b.e7472@gmail.com>
2025-03-05 16:22:18 +0900

    [add] 勤怠エラー通知機能を追加

--- components/usecase.py
+++ components/usecase.py
@@ -33,6 +33,26 @@ def record_time(record_type: RecordType, employee_key):
     requester.post("/daily-workings/timerecord/{}".format(employee_key), payload)
 
 
+def get_daily_timacard_data(from_date=None, to_date=None):
+    """
+    日別勤怠データを取得する
+    
+    Args:
+        from_date: 取得開始日付（YYYY-MM-DD形式）。指定がない場合は今日の日付
+        to_date: 取得終了日付（YYYY-MM-DD形式）。指定がない場合はfrom_dateと同じ日付
+        
+    Returns:
+        辞書型の日別勤怠データ
+    """
+    requester = KOTRequester()
+    start_date = from_date if from_date else _get_working_date()
+    end_date = to_date if to_date else start_date
+
+    uri = f"/daily-workings?&start={start_date}&end={end_date}&additionalFields=currentDateEmployee"
+    
+    return requester.get(uri)
+
+
 def _get_working_date():
     """
     return today formatting '%Y-%m-%d'.
